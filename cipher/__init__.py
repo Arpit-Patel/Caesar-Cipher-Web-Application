ef3c87981755d42f7530e1a6b988fdfcde34fb44
Arpit Patel <a285pate@uwaterloo.ca>
2017-01-16 09:07:21 +0000

    decoding added

--- cipher/__init__.py
+++ cipher/__init__.py
@@ -19,11 +19,25 @@ def encode():
       else:
         output += char
     return render_template("encoded.html", key=key, text=text, output=output);
-  # else:
-   #  return "<h1>Invalid Request</h1>";
 
-#@app.route("/encoded", methods=['GET', 'POST'])
-
+@app.route("/decode", methods=['GET', 'POST'])
+def encode():
+  if request.method == 'GET':
+    return render_template("decode.html");
+  elif request.method == 'POST':
+    text = request.form['text'];
+    key = request.form['key'];
+    intKey = int(key)
+    VALID_STRING = 'abcdefghijklmnopqrstuvwxyz'
+    decrypt_string = 'zyxwvutsrqponmlkjihgfedcba' * 2
+    output = ''
+    for char in text:
+      if char in VALID_STRING:
+        index = decrypt_string.index(str(char))
+        output += decrypt_string[index + intKey]
+      else:
+        output += char
+    return render_template("decoded.html", key=key, text=text, output=output);
 
 if __name__ == "__main__":
   app.run()
